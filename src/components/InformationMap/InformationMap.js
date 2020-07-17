7421b6c8e17faa9601cf67dc08a0f83829f5bccb
Achiaga <alfonso.achiaga@gmail.com>
2020-07-17 15:54:25 +0200

    release v1

--- src/components/InformationMap/InformationMap.js
+++ src/components/InformationMap/InformationMap.js
@@ -6,7 +6,7 @@ const InformationMapWrapper = styled.div`
 	display: flex;
 	justify-content: center;
 	align-items: center;
-	color: white;
+	color: ${(props) => (props.theme === 'light' ? 'palevioletred' : 'white')};
 	margin-bottom: 3em;
 	font-size: 26px;
 `;
@@ -21,16 +21,36 @@ const InformationBox = styled.div`
 	align-items: center;
 `;
 
-const InformationMap = () => {
+const InformationMap = ({ countryCode, results, theme }) => {
+	let averageCountryTraveled = 5.29;
+	let userCountrytraveled = Object.keys(countryCode).length;
+	let countryRate = userCountrytraveled / averageCountryTraveled;
+	let explored = (userCountrytraveled * 100) / 251;
+	countryRate = countryRate.toFixed(1);
+	explored = explored.toFixed(1);
+	let travelerRank = '';
+	console.log(explored);
+	console.log(userCountrytraveled);
+
+	if (userCountrytraveled <= 2) travelerRank = 'Coach Potato  🥔 ';
+	if (userCountrytraveled > 2 && userCountrytraveled <= 5)
+		travelerRank = 'Young Scout 🐿 ';
+	if (userCountrytraveled > 5 && userCountrytraveled <= 10)
+		travelerRank = 'Adventurous ⛺️⛵️ ';
+	if (userCountrytraveled > 10) travelerRank = 'Phileas Fogg 🧑‍🚀 🚀 ';
+
 	return (
-		<InformationMapWrapper>
+		<InformationMapWrapper theme={theme}>
 			<InformationBox>
-				27 <br /> COUNTRIES
+				{userCountrytraveled} <br /> Countries
 			</InformationBox>
 			<InformationBox>
-				20% <br /> of the world explored
+				{explored}% <br /> of the world explored
 			</InformationBox>
-			<InformationBox>Young Scout 🐿</InformationBox>
+			<InformationBox>
+				x{countryRate} <br /> over average person
+			</InformationBox>
+			<InformationBox>{travelerRank}</InformationBox>
 		</InformationMapWrapper>
 	);
 };
